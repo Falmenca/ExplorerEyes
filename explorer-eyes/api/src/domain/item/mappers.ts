c4339915b2d9dc102d4bfe36c807a707e20e4218
Falmenca <enriquedelacruz2436@gmail.com>
2025-10-08 16:47:46 -0700

    Re-organize project structure. Moved API code to main project directory and set up new API routes.

--- explorer-eyes/api/src/domain/item/mappers.ts
+++
@@ -1,7 +0,0 @@
-import { Item as ItemModel } from '@prisma/client'
-import { Item } from './schemas'
-
-export const mapItemModelToEntity = (model: ItemModel): Item => ({
-  id: model.id,
-  name: model.name,
-})
--- explorer-eyes/api/src/integrations/prisma.ts
+++
@@ -1,5 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-
-export const prisma = new PrismaClient()
-
-process.on('exit', () => prisma.$disconnect())

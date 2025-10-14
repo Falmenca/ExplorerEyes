11936675336dd8c56e4e4c4f6892ba7f3200b78d
Falmenca <enriquedelacruz2436@gmail.com>
2025-10-14 14:46:30 -0700

    Normalize case: Item.ts -> item.ts

--- explorer-eyes/src/app/src/app/RootProvider.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from 'react'
-
-export default function RootProvider({ children }: PropsWithChildren) {
-  // add global context providers here
-
-  return <>{children}</>
-}

dfb9d52bbf072691224687efaba1dab2cb118d98
Falmenca <enriquedelacruz2436@gmail.com>
2025-11-06 14:28:58 -0800

    Updated README and Weather API route

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # ExplorerEyes
 UNLV Senior Design 2025
 
+Production Website: https://explorer-eyes.vercel.app/
+
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
 
 ## Getting Started
--- explorer-eyes/src/app/api/health/route.ts
+++ explorer-eyes/src/app/api/health/route.ts
@@ -1,5 +1,11 @@
 // app/api/weather/route.ts
+// Weather API Documentation:
+// https://weather-gov.github.io/api/general-faqs
+// https://www.weather.gov/documentation/services-web-api 
+
 export const runtime = "nodejs";
+
+const BASE_URL = "https://api.weather.gov";
 
 export async function GET(req: Request) {
   // Simple stub so your page has something to render
